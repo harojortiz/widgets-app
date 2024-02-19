5b00eca2691055785508a4e1e27597e83779cc3b
harojortiz <144560786+harojortiz@users.noreply.github.com>
2024-02-19 15:58:22 -0500

    rutas con nombres implementados

--- lib/presentation/screens/buttons/buttons_screen.dart
+++ lib/presentation/screens/buttons/buttons_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 class ButtonScreen extends StatelessWidget {
+  static const String name = 'buttons_screen';
+
   const ButtonScreen({super.key});
 
   @override
