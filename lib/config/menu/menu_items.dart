44e574715b93da1fdd91f410d056084a6ca4fd8e
harojortiz <144560786+harojortiz@users.noreply.github.com>
2024-02-27 09:34:51 -0500

    animated container terminado

--- lib/config/menu/menu_items.dart
+++ lib/config/menu/menu_items.dart
@@ -34,4 +34,9 @@ const appMenuItems = <MenuItems>[
       subTitle: 'Indicadores en pantalla',
       link: '/snackbars',
       icon: Icons.info_outline),
+  MenuItems(
+      title: 'Animated container',
+      subTitle: 'Staful widget animado',
+      link: '/animated',
+      icon: Icons.check_box_outline_blank_rounded),
 ];
--- lib/presentation/screens/animated/animated_screen.dart
+++ lib/presentation/screens/animated/animated_screen.dart
@@ -1,12 +1,58 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
-class AnimatedScreen extends StatelessWidget {
+class AnimatedScreen extends StatefulWidget {
   static const name = 'animated_screen';
 
   const AnimatedScreen({super.key});
 
   @override
+  State<AnimatedScreen> createState() => _AnimatedScreenState();
+}
+
+class _AnimatedScreenState extends State<AnimatedScreen> {
+  double width = 50;
+  double heigth = 50;
+  Color color = Colors.indigo;
+  double borderRadius = 10.0;
+
+  void changeShape() {
+    final random = Random();
+    width = random.nextInt(300) + 120;
+    heigth = random.nextInt(300) + 120;
+    borderRadius = random.nextInt(100) + 20;
+    color = Color.fromRGBO(
+        random.nextInt(255), //red
+        random.nextInt(255), //green
+        random.nextInt(255), //bue
+        1 //opacidad
+        );
+
+    setState(() {});
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Animated container'),
+      ),
+      body: Center(
+        child: AnimatedContainer(
+          duration: const Duration(milliseconds: 400),
+          curve: Curves.elasticOut,
+          width: width <= 0 ? 0 : width,
+          height: heigth <= 0 ? 0 : heigth,
+          decoration: BoxDecoration(
+              color: color,
+              borderRadius:
+                  BorderRadius.circular(borderRadius < 0 ? 0 : borderRadius)),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: changeShape,
+        child: const Icon(Icons.play_arrow_rounded),
+      ),
+    );
   }
 }
